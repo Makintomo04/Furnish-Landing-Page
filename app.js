1a118d4e0f8b3a2491d76670abddee06fab226c4
Makintomo04 <33154227+Makintomo04@users.noreply.github.com>
2020-06-16 01:14:00 +0100

    services section started and backtotop functionality added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const backToTop = document.querySelector("#btt");
+window.onscroll = function () {
+  scrollFunction();
+};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+backToTop.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
+});
