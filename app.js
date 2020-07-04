40b8a3b7b0db0cae8244d6e959fa875130a87b6b
Makintomo04 <33154227+Makintomo04@users.noreply.github.com>
2020-07-04 01:44:11 +0100

    footer section added + improved responsive layout

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const backToTop = document.querySelector("#btt");
+const ham = document.querySelector("#ham");
+const links = document.querySelectorAll(".nav-link");
+const linksUl = document.querySelectorAll(".nav-links");
 window.onscroll = function () {
   scrollFunction();
 };
@@ -12,4 +15,20 @@ function scrollFunction() {
 }
 backToTop.addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
+});
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", (e) => {
+    linksUl[0].classList.remove("nav-reveal");
+    // if (e) {
+    //   links[i].classList.add("active");
+    // } else {
+    //   links[i].classList.remove("active");
+    // }
+  });
+}
+
+ham.addEventListener("click", (e) => {
+  console.log("clicked");
+  linksUl[0].classList.toggle("nav-reveal");
 });
